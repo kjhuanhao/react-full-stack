41ae9521a6f5c71fe527e67aa8148ef06f855d50
jiahao <2077780946@qq.com>
2024-04-27 01:35:32 +0800

    config: 使用 cz-git 简化 commit 操作

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,39 +1,74 @@
 export default {
-  extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
-      // type枚举
       2,
       'always',
-      [
-        'build', // 编译相关的修改，例如发布版本、对项目构建或者依赖的改动
-        'feat', // 新功能
-        'fix', // 修补bug
-        'docs', // 文档修改
-        'style', // 代码格式修改, 注意不是 css 修改, 例如删除了空格之类的
-        'refactor', // 重构
-        'perf', // 优化相关，比如提升性能、体验
-        'test', // 测试用例修改
-        'revert', // 代码回滚
-        'ci', // 持续集成修改
-        'config', // 配置修改
-        'chore' // 其他改动
-      ]
+      ['feat', 'fix', 'config', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'revert', 'chore']
+    ]
+  },
+  prompt: {
+    alias: { fd: 'docs: fix typos' },
+    messages: {
+      type: '选择你要提交的类型 :',
+      scope: '选择一个提交范围（可选）:',
+      customScope: '请输入自定义的提交范围 :',
+      subject: '填写简短精炼的变更描述 :\n',
+      body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
+      breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
+      footerPrefixesSelect: '选择关联issue前缀（可选）:',
+      customFooterPrefix: '输入自定义issue前缀 :',
+      footer: '列举关联issue (可选) 例如: #31, #I3244 :\n',
+      confirmCommit: '是否提交或修改commit ?'
+    },
+    types: [
+      { value: 'feat', name: 'feat:     新增功能 | A new feature' },
+      { value: 'fix', name: 'fix:      修复缺陷 | A bug fix' },
+      { value: 'config', name: 'config:   配置变更 | Changes to the configuration files' },
+      { value: 'docs', name: 'docs:     文档更新 | Documentation only changes' },
+      { value: 'style', name: 'style:    代码格式 | Changes that do not affect the meaning of the code' },
+      { value: 'refactor', name: 'refactor: 代码重构 | A code change that neither fixes a bug nor adds a feature' },
+      { value: 'perf', name: 'perf:     性能提升 | A code change that improves performance' },
+      { value: 'test', name: 'test:     测试相关 | Adding missing tests or correcting existing tests' },
+      { value: 'build', name: 'build:    构建相关 | Changes that affect the build system or external dependencies' },
+      { value: 'ci', name: 'ci:       持续集成 | Changes to our CI configuration files and scripts' },
+      { value: 'revert', name: 'revert:   回退代码 | Revert to a commit' },
+      { value: 'chore', name: 'chore:    其他修改 | Other changes that do not modify src or test files' }
     ],
-    'type-empty': [2, 'never'], // never: type不能为空; always: type必须为空
-    'type-case': [0, 'always', 'lower-case'], // type必须小写，upper-case大写，camel-case小驼峰，kebab-case短横线，pascal-case大驼峰，等等
-    'scope-empty': [0],
-    'scope-case': [0],
-    'subject-empty': [2, 'never'], // subject不能为空
-    'subject-case': [0],
-    'subject-full-stop': [0, 'never', '.'], // subject以.为结束标记
-    'header-max-length': [2, 'always', 72], // header最长72
-    'body-leading-blank': [0], // body换行
-    'footer-leading-blank': [0, 'always'] // footer以空行开头
+    useEmoji: false,
+    emojiAlign: 'center',
+    useAI: false,
+    aiNumber: 1,
+    themeColorCode: '',
+    scopes: [],
+    allowCustomScopes: true,
+    allowEmptyScopes: true,
+    customScopesAlign: 'bottom',
+    customScopesAlias: 'custom',
+    emptyScopesAlias: 'empty',
+    upperCaseSubject: false,
+    markBreakingChangeMode: false,
+    allowBreakingChanges: ['feat', 'fix'],
+    breaklineNumber: 100,
+    breaklineChar: '|',
+    skipQuestions: [],
+    issuePrefixes: [
+      // 如果使用 gitee 作为开发管理
+      { value: 'link', name: 'link:     链接 ISSUES 进行中' },
+      { value: 'closed', name: 'closed:   标记 ISSUES 已完成' }
+    ],
+    customIssuePrefixAlign: 'top',
+    emptyIssuePrefixAlias: 'skip',
+    customIssuePrefixAlias: 'custom',
+    allowCustomIssuePrefix: true,
+    allowEmptyIssuePrefix: true,
+    confirmColorize: true,
+    maxHeaderLength: Infinity,
+    maxSubjectLength: Infinity,
+    minSubjectLength: 0,
+    scopeOverrides: undefined,
+    defaultBody: '',
+    defaultIssues: '',
+    defaultScope: '',
+    defaultSubject: ''
   }
 }
-
-// git commit -m <type>[optional scope]: <description> //注意冒号后面有空格
-// - type：提交的改动类型（如新增、修改、更新等）
-// - optional scope：标识此次提交主要涉及到代码中哪个模块
-// - description：一句话描述此次提交的主要内容
